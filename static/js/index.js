828116ee58221bd6f26e94c6fe57753066041d62
Ajit_Patil <patilajit020@gmail.com>
2022-07-28 15:58:14 +0530

    Added about blog minia

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,19 @@
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
-btn.addEventListener("click",()=>{
-    btn.classList.toggle("open");
-    nav.classList.toggle("flex");
-    nav.classList.toggle("hidden");
-})
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
+
+const popupOverlay = document.querySelector(".popup-overlay");
+const popupClose = document.querySelector(".popup-close");
+
+popupClose.addEventListener("click", () => {
+  popupOverlay.style.display = "none";
+});
+
+setTimeout(() => {
+  popupOverlay.style.display = "block";
+}, 2000);
